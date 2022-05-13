8d836690381cb96663f8dedb3715ad7a96b2f11a
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2022-05-13 22:50:14 +0200

    Add files via upload

---
+++ task_08.py
@@ -0,0 +1,15 @@
+#This function will convert any number into hours and minutes 
+def time_conv(time):
+    hours = time // 60
+    minutes = time % 60 
+    if hours == 1 and minutes == 1:
+        print(f"{hours} hour, {minutes} minute")
+    elif hours == 1 and minutes >= 0:
+        print(f"{hours} hour, {minutes} minutes")
+    elif hours >= 0 and minutes == 1:
+        print(f"{hours} hours, {minutes} minute")
+    else:
+        print(f"{hours} hours, {minutes} minutes")
+
+
+time_conv(120)        
