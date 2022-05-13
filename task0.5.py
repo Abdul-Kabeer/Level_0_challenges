198cf7e4c704556bbdd77b25e7bef1ffb603f814
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2022-05-13 22:45:51 +0200

    Delete task0.5.py

--- task0.5.py
+++
@@ -1,8 +0,0 @@
-def triangle_area(side1, side2, side3): 
-    '''This will print the area of a trianlge using the side lengths method'''
-    print ('The area of this triangle is:')
-    return (side1 + side2 + side3) * 1/2
-
-result = triangle_area(3, 4, 5)
-
-print(result)
