3f3eccf02ad9d211819cab411555cb90833abaed
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2022-05-13 22:46:14 +0200

    Delete task0.8.py

--- task0.8.py
+++
@@ -1,8 +0,0 @@
-#This function will convert any number into hours and minutes 
-def time_conv(x):
-    return (x // 60) + (x % 60) 
-
-x = 133
-hours = (x // 60)
-minutes = (x % 60)
-print (hours , 'hours', minutes , 'minutes')
