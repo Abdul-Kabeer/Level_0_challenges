8e39779f50e34bba1bff2808f8fa3b5930d8ca04
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2022-05-18 12:41:59 +0200

    Add files via upload

---
+++ task_07.py
@@ -0,0 +1,9 @@
+def celcius_to_fahrenheit(cel):
+    return 9/5 * cel + 32
+
+print(celcius_to_fahrenheit(36))
+
+def fahrenheit_to_celcius(fahren):
+    return (fahren - 32) * 5/9
+
+print(fahrenheit_to_celcius(96.8))
