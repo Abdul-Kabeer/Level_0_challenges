71e64254049af3d4126ea2a0429618510e9c20df
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2022-05-18 12:37:24 +0200

    Delete task_05.py

--- task_05.py
+++
@@ -1,8 +0,0 @@
-def area_triangle(side1, side2, side3): 
-    semiperimeter = (side1 + side2 + side3) / 2 
-    area = (semiperimeter*(semiperimeter-side1)*(semiperimeter-side2)*(semiperimeter-side3)) ** 0.5
-    return area
-
-area_tri = area_triangle(3,4,5)
-
-print (f'The area of this triangle is: {area_tri}')
